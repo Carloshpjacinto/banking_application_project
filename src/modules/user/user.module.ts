e8de320f11caf87f179711088249cee59f34f7a4
Carloshpjacinto <carlo.henrique37@gmail.com>
2025-05-19 13:34:37 -0300

    feature/module-user

---
+++ src/modules/user/user.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { UserController } from './controller/user.controller';
+import { DatabaseModule } from 'src/database/database.module';
+import { CreateUserService } from './services/CreateUser.service';
+import { userProviders } from './user.providers';
+
+@Module({
+  imports: [DatabaseModule],
+  controllers: [UserController],
+  providers: [CreateUserService, ...userProviders],
+})
+export class UserModule {}
